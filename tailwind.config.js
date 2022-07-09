48a9f8d936e96467f61ccd05b37db6398d66b153
hossein <samanihossein1379@gmail.com>
2022-07-09 13:31:25 +0430

    custom colors added to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   purge: [],
   darkMode: 'class',
   theme: {
+    colors: {
+      prime: '#2563EB',
+      prime_h: '#0E3B9C',
+      'gray-label': '#757575',
+      white: '#ffffff',
+      'white-100': '#F9F9F9',
+    },
     extend: {},
   },
   variants: {
