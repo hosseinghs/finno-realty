57d01a14c95c0580615f62f02b22a1f865623f55
Hossein Samani <samanihossein1379@gmail.com>
2022-07-09 22:56:04 +0430

    reset password page added

---
+++ pages/auth/reset-password.vue
@@ -0,0 +1,18 @@
+<template>
+  <AuthCard>
+    <AuthFinnoRealty />
+    <ResetPasswordStepOne />
+    <ResetPasswordStepTwo />
+    <ResetPasswordStepThree />
+  </AuthCard>
+</template>
+
+<script>
+export default {
+  methods: {
+    submitForm() {
+      console.log('ok')
+    },
+  },
+}
+</script>
