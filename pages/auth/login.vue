41f65ce85bcd83bfaecb676c035740f8edbe2e39
hossein <samanihossein1379@gmail.com>
2022-07-09 13:33:17 +0430

    login page created

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,9 +1,67 @@
 <template>
-  <div>login</div>
+  <div>
+    <AuthCard>
+      <div class="text-prime text-center mb-10">
+        <h1 class="font-bold mb-1 text-2xl">فینوریلتی</h1>
+        <span class="font-normal text-lg"> پلتفرم مدیریت املاک </span>
+      </div>
+
+      <div>
+        <form @submit.prevent="submitForm()">
+          <FormText
+            icon
+            type="text"
+            placeholder="شماره موبایل/ نام کاربری/ ایمیل"
+          />
+          <FormText
+            placeholder="رمز عبور"
+            :type="showPassword ? 'text' : 'password'"
+            autocomplete
+            :icon="
+              showPassword
+                ? `${require('~/assets/img/icon/eye-off.svg')}`
+                : `${require('~/assets/img/icon/eye.svg')}`
+            "
+            @iconClick="showPassword = !showPassword"
+          />
+          <div class="text-gray-label text-left text-sm mt-2">
+            <span class="cursor-pointer"> رمز عبور خود را فراموش کرده ام </span>
+          </div>
+
+          <FormBtnPrime class="mt-20">
+            <span class="font-bold"> ورود </span>
+          </FormBtnPrime>
+          <div class="text-center mt-6 font-normal text-sm">
+            <p class="text-gray-label">
+              هنوز ثبت نام نکرده اید؟
+              <span class="text-prime cursor-pointer hover:text-prime_h">
+                ثبت نام کنید
+              </span>
+            </p>
+          </div>
+        </form>
+      </div>
+    </AuthCard>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      showPassword: false,
+    }
+  },
+
+  methods: {
+    submitForm() {
+      console.log('ok')
+    },
+    log(e) {
+      console.log(e)
+    },
+  },
+}
 </script>
 
 <style></style>
