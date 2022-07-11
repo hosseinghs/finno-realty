af4725b895a2deedd44270ddb6d3448d68de6e83
hossein <samanihossein1379@gmail.com>
2022-07-11 15:31:32 +0430

    dark mode added

--- components/form/Text.vue
+++ components/form/Text.vue
@@ -1,13 +1,13 @@
 <template>
   <div
     id="input_wrapper"
-    class="relative shadow rounded-lg px-4 py-2 bg-white my-6 transition-all duration-100"
+    class="relative shadow rounded-lg px-4 py-2 dark:border dark:border-white my-6 transition-all duration-100"
     :class="
       activeFocus ? 'border-2 border-prime_active' : 'border-2 border-white'
     "
   >
     <input
-      class="w-full text-gray-label text-gray-label::placeholder outline-none"
+      class="w-full text-gray-label dark:text-white bg-transparent placeholder-text-gray-label dark:placeholder-white outline-none"
       v-bind="$attrs"
       @focus="activeFocus = true"
       @blur="activeFocus = false"
@@ -44,5 +44,9 @@ export default {
   left: 10px;
   top: 50%;
   transform: translateY(-50%);
+}
+
+#input_wrapper input {
+  background-color: transparent;
 }
 </style>
