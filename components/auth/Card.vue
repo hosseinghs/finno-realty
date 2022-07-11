6edcc9d905f3f7de19adf501e11f9d53901782ae
hossein <samanihossein1379@gmail.com>
2022-07-11 15:14:40 +0430

    dark mode fixed

--- components/auth/Card.vue
+++ components/auth/Card.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="w-full relative bg-white-100 shadow-md px-10 py-16 rounded-lg">
+  <div
+    class="w-full relative bg-white-100 shadow-md px-10 py-16 rounded-lg dark:bg-black-100"
+  >
     <slot />
   </div>
 </template>
