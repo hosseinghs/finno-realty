dcef3522576e399d8aa9169759733e8b40b0ef7e
hossein <samanihossein1379@gmail.com>
2022-07-11 17:00:26 +0430

    btn ui fixed

--- components/form/Btn/Prime.vue
+++ components/form/Btn/Prime.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     class="w-full px-6 py-2 text-white bg-prime hover:bg-prime_h transition-all duration-200 rounded-lg"
-    :class="isLoading ? 'opacity-40  disabled:text-[#CBCBCB]' : ''"
+    :class="isLoading ? 'opacity-40  disabled:text-[#CBCBCB] cursor-default' : ''"
     :disabled="isLoading"
   >
     <div class="flex items-center justify-center">
