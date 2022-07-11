b1d5df1096403bf4782a861015a7f68c32382990
hossein <samanihossein1379@gmail.com>
2022-07-11 15:39:58 +0430

    placeholder fixed

--- components/auth/resetPassword/StepOne.vue
+++ components/auth/resetPassword/StepOne.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <form @submit.prevent="submitForm()">
-      <FormText icon type="text" placeholder="موبایل/ نام کاربری/ ایمیل" />
+      <FormText icon type="text" placeholder="شماره موبایل خود را وارد کنید" />
 
       <FormBtnPrime type="submit" class="mt-5">
         <span class="font-bold">ارسال کد تایید</span>
