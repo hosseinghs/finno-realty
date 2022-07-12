2e746e58759e1020531dffcff6245a2d40b4b05c
hossein <samanihossein1379@gmail.com>
2022-07-12 11:08:57 +0430

    notif position fixed

---
+++ components/ui/Notif.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    id="notif_component"
+    class="fixed top-5 left-1/2 transform -translate-x-1/2 w-80 px-4 bg-prime_h z-30"
+  >
+    <div class="flex items-center justify-end">
+      <FormBtnIconClose />
+    </div>
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Et libero quod
+      enim molestias quasi, ullam, unde culpa sequi veniam suscipit dicta,
+      ducimus a impedit totam nostrum voluptatibus ad adipisci at.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
