d0185802f4d7d805751000069ebf22a39b97e09f
hossein <samanihossein1379@gmail.com>
2022-07-13 12:12:19 +0430

    StepTwo component created

---
+++ components/auth/signup/StepTwo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt, tempora enim
+    delectus optio minus aut officiis necessitatibus doloremque nihil
+    dignissimos veniam nulla amet! Dolorum aut sed deleniti qui at laudantium?
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
