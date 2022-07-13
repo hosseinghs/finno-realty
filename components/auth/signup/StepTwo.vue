75788863c317729aa61a2d0f7e30a52d73d888d5
hossein <samanihossein1379@gmail.com>
2022-07-13 13:40:17 +0430

    otp component added to sign up step 2

--- components/auth/signup/StepTwo.vue
+++ components/auth/signup/StepTwo.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt, tempora enim
-    delectus optio minus aut officiis necessitatibus doloremque nihil
-    dignissimos veniam nulla amet! Dolorum aut sed deleniti qui at laudantium?
+    <form @submit.prevent="submitForm()">
+      <FormOtp />
+    </form>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    submitForm() {
+      console.log('ok')
+    },
+  },
+}
 </script>
