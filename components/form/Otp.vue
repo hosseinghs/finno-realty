6430b4d2f2e45b0af3f74631221898baee420077
hossein <samanihossein1379@gmail.com>
2022-07-19 14:03:53 +0430

    dark mode added

--- components/form/Otp.vue
+++ components/form/Otp.vue
@@ -11,8 +11,8 @@
       type="text"
       maxlength="1"
       pattern="/d*"
-      class="w-7 text-center bg-white-100 text-gray-label border-b border-prime outline-none"
-      :class="i !== 1 || i !== 6 ? 'mx-2' : ''"
+      class="w-7 text-center bg-white-100 text-gray-label border-b-2 border-prime dark:border-white dark:bg-black-100 outline-none"
+      :class="i !== 1 || i !== 6 ? 'mx-1' : ''"
       @keyup="onInput({ e: $event, i })"
     />
   </div>
