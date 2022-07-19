ee1a9f3fb38e676e12c8f6780822e7c273d98609
hossein <samanihossein1379@gmail.com>
2022-07-19 14:28:35 +0430

    StepTwo component fixed

--- components/auth/signup/StepTwo.vue
+++ components/auth/signup/StepTwo.vue
@@ -4,6 +4,20 @@
       <div class="w-56 mx-auto">
         <FormOtp />
         <FormTimer />
+        
+      </div>
+      <FormBtnPrime>ثبت نام</FormBtnPrime>
+
+      <div class="text-center mt-6 font-normal text-sm">
+        <p class="text-gray-label dark:text-white">
+          قبلا ثبت نام کرده اید؟
+          <span
+            class="text-prime font-bold cursor-pointer hover:text-prime_h"
+            @click.stop="$router.push({ name: 'auth-login' })"
+          >
+            اینجا را کلیک کنید
+          </span>
+        </p>
       </div>
     </form>
   </div>
