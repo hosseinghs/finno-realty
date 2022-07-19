b6a4dbb7f098fd0eb8f08aaf13c2a75f4ef46bd3
hossein <samanihossein1379@gmail.com>
2022-07-19 15:41:20 +0430

    push

---
+++ pages/auth/profile.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ul class="flex items-center">
+      <li class="w-full" v-for="(item, i) in items" :key="i">
+        {{ item.text }}
+      </li>
+    </ul>
+    <ul>
+      <li v-for="(item, i) in items" :key="i">
+        {{ i }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [{ text: 'test 1' }, { text: 'test 2' }, { text: 'test 3' }],
+    }
+  },
+}
+</script>
+
+<style></style>
