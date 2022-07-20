1f3705a271b12fbd64268c6c0f0e672d7731e992
hossein <samanihossein1379@gmail.com>
2022-07-20 14:52:26 +0430

    Dropdown component created

---
+++ components/ui/Dropdown.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="dropdown">
+    <button
+      class="flex items-center justify-center text-prime font-semibold text-sm"
+    >
+      <div class="ml-2">
+        {{ text }}
+      </div>
+      <img :src="require('~/assets/img/icon/arrow-down.svg')" />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+
