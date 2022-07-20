3098e5958b3649e1bd4e9b4d92a84794bd2737b6
hossein <samanihossein1379@gmail.com>
2022-07-20 15:31:39 +0430

    dropdown bug

--- components/ui/Dropdown.vue
+++ components/ui/Dropdown.vue
@@ -1,25 +1,59 @@
 <template>
   <div id="dropdown">
-    <button
-      class="flex items-center justify-center text-prime font-semibold text-sm"
-    >
-      <div class="ml-2">
-        {{ text }}
+    <div class="z-50">
+      <div @click="boz">
+        <button
+          class="flex items-center justify-center text-prime font-semibold text-sm"
+        >
+          <div class="ml-2">{{ title }} {{ showDropdown }}</div>
+          <img :src="require('~/assets/img/icon/arrow-down.svg')" />
+        </button>
       </div>
-      <img :src="require('~/assets/img/icon/arrow-down.svg')" />
-    </button>
+      <div v-if="showDropdown">
+        <ul>
+          <li>item</li>
+          <li>item</li>
+          <li>item</li>
+          <li>item</li>
+          <li>item</li>
+          <li>item</li>
+        </ul>
+      </div>
+    </div>
+    <div class="w-full h-screen fixed top-0 left-0 z-10"></div>
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    text: {
+    title: {
       type: String,
       default: '',
+    },
+
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  computed: {
+    showDropdown: {
+      get() {
+        return this.visible
+      },
+      set(v) {
+        return v
+      },
+    },
+  },
+
+  methods: {
+    boz() {
+      this.showDropdown = true
+      console.log('boz')
     },
   },
 }
 </script>
-
-
