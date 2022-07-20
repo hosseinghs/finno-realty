773a5ea3b22250661fccf1e6557937303756eefb
hossein <samanihossein1379@gmail.com>
2022-07-20 10:10:14 +0430

    save Btn added

---
+++ components/form/Btn/Save.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    class="w-full px-6 py-2 text-orange bg-transparent hover:text-white hover:bg-orange border-2 border-orange transition-all duration-200 rounded-lg"
+    :class="
+      isLoading ? 'opacity-40  disabled:text-[#CBCBCB] cursor-default' : ''
+    "
+    :disabled="isLoading"
+  >
+    <div class="flex items-center justify-center">
+      <slot />
+      <UiLoading v-if="isLoading" color="#EC860E" />
+    </div>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
