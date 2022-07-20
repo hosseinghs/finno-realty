efb7bffc9b544ac5c9cc194437a64a3e18718784
hossein <samanihossein1379@gmail.com>
2022-07-20 12:37:11 +0430

    Form Component created

---
+++ components/form/Form.vue
@@ -0,0 +1,27 @@
+<template>
+  <form :ref="name" @submit.prevent="submit">
+    <slot />
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: '',
+      required: true,
+    },
+  },
+
+  methods: {
+    submit() {
+      const name = this.name
+      const formElem = this.$refs[name]
+      const inputsArr = [...formElem.querySelectorAll('#input_wrapper')]
+      const classLists = inputsArr.map((input) => [...input.classList])
+      console.log(classLists);
+    },
+  },
+}
+</script>
