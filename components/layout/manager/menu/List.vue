701dfd015b6192bdd1e0c7ae98599701c5948661
Hossein Ghebadi <job.hghs@gmail.com>
2022-07-23 12:27:24 +0430

    feat: menuItems added

--- components/layout/manager/menu/List.vue
+++ components/layout/manager/menu/List.vue
@@ -1,11 +1,31 @@
 <template>
   <ul>
-    <LayoutMenuItem />
+    <LayoutManagerMenuItem
+      v-for="item in menuItems"
+      :key="item.id"
+      :title="item.title"
+    />
   </ul>
 </template>
 
 <script>
-export default {}
-</script>
+export default {
+  name: 'SideBarMenuListComponent',
 
-<style></style>
+  data() {
+    return {
+      menuItems: [
+        { id: 1, title: 'داشبورد', route: '', name: '' },
+        { id: 2, title: 'پروفایل', route: '', name: '' },
+        { id: 3, title: 'مجتمع ها', route: '', name: '' },
+        { id: 4, title: 'ارتباطات', route: '', name: '' },
+        { id: 5, title: 'تعمیرات و نگهداری', route: '', name: '' },
+        { id: 6, title: 'درآمدها و هزینه ها', route: '', name: '' },
+        { id: 7, title: 'کیف پول', route: '', name: '' },
+        { id: 8, title: 'گزارش ها', route: '', name: '' },
+        { id: 9, title: 'تنظیمات', route: '', name: '' },
+      ],
+    }
+  },
+}
+</script>
