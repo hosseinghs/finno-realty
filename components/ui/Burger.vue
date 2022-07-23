0db65f9131b2001aff881ed798fbc582ce361826
Hossein Ghebadi <job.hghs@gmail.com>
2022-07-23 13:33:55 +0430

    feat: burger component added

---
+++ components/ui/Burger.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="burgur" class="cursor-pointer" @click="$emit('burgerClick')">
+    <img :src="require('~/assets/img/burger.svg')" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BurgerComponent',
+}
+</script>
