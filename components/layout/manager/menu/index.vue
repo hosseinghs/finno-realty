c70eef4227494b0bb2158023465a49a1f8e5a9d9
Hossein Ghebadi <job.hghs@gmail.com>
2022-07-23 13:53:43 +0430

    feat: mobileMenu added to menu component

--- components/layout/manager/menu/index.vue
+++ components/layout/manager/menu/index.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-    <LayoutManagerMenuDesktop />
-    <!-- <LayoutManagerMenuMobile /> -->
+    <div class="hidden md:block">
+      <LayoutManagerMenuDesktop />
+    </div>
+    <div class="md:hidden" v-if="isMobileSidebarVisible">
+      <LayoutManagerMenuMobile />
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'SideBarComponent',
+
+  computed: {
+    ...mapState(['isMobileSidebarVisible']),
+  },
 }
 </script>
