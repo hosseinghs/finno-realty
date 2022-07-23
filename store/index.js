6c6ad7f142b21f2ee007162556ec3857286bbd25
Hossein Ghebadi <job.hghs@gmail.com>
2022-07-23 13:38:51 +0430

    feat: setMobileSidebarState action added

--- store/index.js
+++ store/index.js
@@ -3,17 +3,24 @@ export default {
 
   state: () => ({
     isModalVisible: false,
+    isMobileSidebarVisible: false,
   }),
 
   mutations: {
     SET_MODAL_STATE(state, bool) {
       state.isModalVisible = bool
     },
+    SET_MOBILE_SIDEBAR_STATE(state, bool) {
+      state.isMobileSidebarVisible = bool
+    },
   },
 
   actions: {
     setModalState({ commit }, bool) {
       commit('SET_MODAL_STATE', bool)
+    },
+    setMobileSidebarState({ commit }, bool) {
+      commit('SET_MOBILE_SIDEBAR_STATE', bool)
     },
   },
 }
