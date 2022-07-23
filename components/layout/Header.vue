87a77c0c050cc7c07e3a9b5780f0e6ca48c83f06
Hossein Ghebadi <job.hghs@gmail.com>
2022-07-23 13:54:50 +0430

    feat: burger component added to header in mobile mode

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -1,6 +1,9 @@
 <template>
   <header class="flex items-center justify-between py-4 px-12">
-    <div class="text-prime">
+    <div class="md:hidden">
+      <UiBurger @burgerClick="setMobileSidebarState(true)" />
+    </div>
+    <div class="text-center md:text-right text-prime">
       <h3 class="text-xl font-bold">فینوریلتی</h3>
       <span class="font-normal">پلتفرم مدیریت املاک</span>
     </div>
@@ -8,18 +11,30 @@
       <div class="flex-1">
         <UiDropdown text="انتخاب نقش" />
       </div>
-      <div class="mx-4">
-        <img :src="require('~/assets/img/notif.svg')" />
-      </div>
-      <div class="">
-        <img :src="require('~/assets/img/logout.svg')" />
+      <div class="hidden md:block">
+        <div class="flex items-center">
+          <div class="mx-4 cursor-pointer">
+            <img :src="require('~/assets/img/notif.svg')" />
+          </div>
+          <div class="cursor-pointer">
+            <img :src="require('~/assets/img/logout.svg')" />
+          </div>
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
-export default {}
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'AppHeaderComponent',
+
+  methods: {
+    ...mapActions(['setMobileSidebarState']),
+  },
+}
 </script>
 
 <style></style>
