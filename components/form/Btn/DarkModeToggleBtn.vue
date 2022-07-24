0b583d74498df630bbda07c3d51f9aa13d4fdbd9
Hossein Ghebadi <job.hghs@gmail.com>
2022-07-24 11:36:01 +0430

    feat: darkModeToggleBtn component completed

--- components/form/Btn/DarkModeToggleBtn.vue
+++ components/form/Btn/DarkModeToggleBtn.vue
@@ -5,14 +5,15 @@
   >
     <div
       id="circle"
-      class="w-5 h-5 absolute top-1/2 transform -translate-y-1/2 transition-all duration-300 rounded-full bg-white cursor-pointer"
-      :class="isNight ? 'right-1' : 'left-1'"
+      class="w-5 h-5 absolute top-1/2 transform -translate-y-1/2 transition-all duration-500 rounded-full bg-white cursor-pointer"
+      :class="isNight ? 'right-1' : 'right-7'"
       @click.stop="toggleMode()"
     ></div>
+
     <div
       id="img_wrapper"
-      class="absolute top-1/2 transform -translate-y-1/2"
-      :class="isNight ? 'left-1' : 'right-1'"
+      class="absolute top-1/2 right-1 transform -translate-y-1/2 transition-all duration-200"
+      :class="isNight ? 'right-7' : 'right-1'"
     >
       <img
         v-show="!isNight"
@@ -41,8 +42,8 @@ export default {
   methods: {
     toggleMode() {
       this.isNight = !this.isNight
-      const htmlElem = document.querySelector('html')
-      htmlElem.classList.toggle('dark')
+        const htmlElem = document.querySelector('html')
+        htmlElem.classList.toggle('dark')
     },
   },
 }
